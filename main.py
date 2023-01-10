9b8941a3e660fa3e8c80a1e104456391e909d3ab
Rebecca Jeffers <r.jeffers@instadeep.com>
2023-01-10 17:06:55 +0100

    change main to return result of function or hard coded float depending on bool

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from file1 import get_my_float_from_imported_package
 
 
-def get_my_float() -> float:
+def get_my_float(mybool: bool) -> float:
     """Obtain a float from an imported function.
 
     Returns:
         a float obtained from an imported function somewhere else.
     """
-    return get_my_float_from_imported_package()
+    if mybool:
+        return get_my_float_from_imported_package()
+    else:
+        return 0.0
 
 
 if __name__ == "__main__":
     print("I am going to print a float")
     print("I've changed the script")
-    print(get_my_float())
+    print(get_my_float(True))
